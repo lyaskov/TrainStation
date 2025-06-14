63d8211deb04634db268ce6dfb29546ac85f4c56
lyaskov <lyaskov1@gmail.com>
2025-06-14 12:53:20 +0300

    Add ViewSets for Station, Route, TrainType, Train, Crew, Journey, Order, and Ticket models

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Station, Route, TrainType, Train, Crew, Journey, Order, Ticket
+)
+from .serializers import (
+    StationSerializer, RouteSerializer, TrainTypeSerializer, TrainSerializer,
+    CrewSerializer, JourneySerializer, OrderSerializer, TicketSerializer
+)
 
-# Create your views here.
+
+class StationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows stations to be viewed or edited.
+    """
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows routes to be viewed or edited.
+    """
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows train types to be viewed or edited.
+    """
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows trains to be viewed or edited.
+    """
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows crew members to be viewed or edited.
+    """
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows journeys to be viewed or edited.
+    """
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows orders to be viewed or edited.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tickets to be viewed or edited.
+    """
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
