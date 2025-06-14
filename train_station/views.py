be63d78b5b41286f30a29bdbda2b6865af509d45
lyaskov <lyaskov1@gmail.com>
2025-06-14 16:18:25 +0300

    Limit Order and Ticket access to current user and auto-assign user

--- train_station/views.py
+++ train_station/views.py
@@ -1,10 +1,16 @@
-from rest_framework import viewsets
-from .models import (
-    Station, Route, TrainType, Train, Crew, Journey, Order, Ticket
-)
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+
+from .models import Station, Route, TrainType, Train, Crew, Journey, Order, Ticket
 from .serializers import (
-    StationSerializer, RouteSerializer, TrainTypeSerializer, TrainSerializer,
-    CrewSerializer, JourneySerializer, OrderSerializer, TicketSerializer
+    StationSerializer,
+    RouteSerializer,
+    TrainTypeSerializer,
+    TrainSerializer,
+    CrewSerializer,
+    JourneySerializer,
+    OrderSerializer,
+    TicketSerializer,
 )
 
 
@@ -12,6 +18,7 @@ class StationViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows stations to be viewed or edited.
     """
+
     queryset = Station.objects.all()
     serializer_class = StationSerializer
 
@@ -20,6 +27,7 @@ class RouteViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows routes to be viewed or edited.
     """
+
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
@@ -28,6 +36,7 @@ class TrainTypeViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows train types to be viewed or edited.
     """
+
     queryset = TrainType.objects.all()
     serializer_class = TrainTypeSerializer
 
@@ -36,6 +45,7 @@ class TrainViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows trains to be viewed or edited.
     """
+
     queryset = Train.objects.all()
     serializer_class = TrainSerializer
 
@@ -44,6 +54,7 @@ class CrewViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows crew members to be viewed or edited.
     """
+
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
@@ -52,6 +63,7 @@ class JourneyViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows journeys to be viewed or edited.
     """
+
     queryset = Journey.objects.all()
     serializer_class = JourneySerializer
 
@@ -60,13 +72,29 @@ class OrderViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows orders to be viewed or edited.
     """
-    queryset = Order.objects.all()
+
     serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows tickets to be viewed or edited.
     """
-    queryset = Ticket.objects.all()
+
     serializer_class = TicketSerializer
+
+    def get_queryset(self):
+        return Ticket.objects.filter(order__user=self.request.user)
+
+    def perform_create(self, serializer):
+        order = serializer.validated_data["order"]
+        if order.user != self.request.user:
+            raise PermissionDenied("You can only create tickets for your own orders.")
+        serializer.save()
