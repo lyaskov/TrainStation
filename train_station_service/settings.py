074c8d65b4feb69b35843482a72815c1cc52cec5
lyaskov <lyaskov1@gmail.com>
2025-06-15 13:03:23 +0300

    Increase pagination and request limits in API settings

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "300/day"},
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
